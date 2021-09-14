61d0c5ddba3a6031eed4a6eb268317e3367b83fc
Elmehdi Amlou <90270530+elmehdiamlou@users.noreply.github.com>
2021-09-14 17:34:41 +0100

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,18 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private router: Router) {}
+  public user!: Observable<User>;
 
+  constructor(private http: HttpClient, private router: Router) {}
+  
   register(user: User) {
     return this.http.post(`${environment.baseUrl}/api/auth/register`, user)
   }
-  
+
   login(username: string, password: string){
     return this.http.post<any>(`${environment.baseUrl}/api/auth/login`, {username, password})
     .pipe(map
