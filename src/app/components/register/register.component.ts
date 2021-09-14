e65319256695f2399b95f7f69c471c7ba82737bb
Elmehdi Amlou <90270530+elmehdiamlou@users.noreply.github.com>
2021-09-14 17:33:16 +0100

    Update register.component.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
+  registerForm!: FormGroup;
+  error!: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService  
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
   }
 
+  get f(){ return this.registerForm.controls;}
+
+  onSubmit(){
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    this.authService.register(this.registerForm.value)
+    .subscribe(
+        data => {
+          console.log('Registration successful');
+            this.router.navigate(['../login']);
+        },
+        error => {
+          console.log("Error !!!");
+          this.error = "Required Field";
+        });
+  }
 }
