1b93f06be16599cf05bd21221e4a1f34d5bdfca0
Elmehdi Amlou <90270530+elmehdiamlou@users.noreply.github.com>
2021-09-28 21:27:28 +0100

    get role from token and not from localstorage to add more security to the app

--- src/app/services/guards/access-guard.guard.ts
+++ src/app/services/guards/access-guard.guard.ts
@@ -1,24 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {  CanActivate } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccessGuardGuard implements CanActivate {
 
   token = localStorage.getItem("token");
-  role = localStorage.getItem("role");
+  constructor(private authService: AuthService, private http: HttpClient){}
+  response!: boolean;
 
-  constructor(private authService: AuthService){}
-
-  canActivate(): boolean{ 
-    
-   if(this.role === 'Admin' && this.token !== null){
-     return true;
-   }
-    this.authService.logout();
-    return false;
-   
+  canActivate(): boolean{  
+    const headers = { 'Authorization': this.token!};
+    this.http.get<any>(`${environment.baseUrl}/api/management/role`, {headers}).subscribe( data => {
+      if(this.token != null && data['message'] == 'Admin'){
+        this.response = true;
+      } else {        
+        this.authService.logout();
+        this.response = false;        
+      }    
+    })
+   return this.response;
  }
-
 }
